18ca060e399a3f7a217ea17ddfda2c8e925b10eb
pappy <pappy@ascelion.com>
2019-10-28 19:31:36 +0100

    updated jenkins pipeline

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,33 +1,4 @@
-pipeline {
-	agent any
-	options {
-		disableConcurrentBuilds()
-		timeout time: 45, unit: 'MINUTES'
-		buildDiscarder logRotator( daysToKeepStr: "14" )
-	}
-	tools {
-		jdk 'J8'
-	}
-	environment {
-		JENKINS_NODE_COOKIE = 'dontkillme'
-	}
 
-	stages {
-		stage('Compile') {
-			steps {
-				sh "git fetch --force --tags"
-				sh "chmod +x gradlew"
-				sh "./gradlew clean"
-				sh "./gradlew classes testClasses"
-			}
-		}
-		stage('Checks') {
-			steps {
-				sh "./gradlew check --continue || true"
-				junit testResults: "**/TEST-*.xml"
-				step([$class: 'Publisher'])
-			}
-		}
-	}
+publicProject {
+	timeout = 15
 }
-
